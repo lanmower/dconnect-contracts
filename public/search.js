f53fb24bf0cbe66e572cb85193633bcef2566d74
Glitch (dconnect-homepage) <none>
2019-02-12 14:17:52 +0000

    🏓😽 Checkpoint ./public/search.js:115759/92

--- public/search.js
+++ public/search.js
@@ -1,16 +1,16 @@
 window.network = {
-    blockchain: 'eos',
-    protocol: 'https',
-    host: 'dconnect.live',
-    port: 443,
-    chainId: '342f1b4af05f4978a8f5d8e3e3e3761cb22dacf21a93e42abe0753bdacb6b621',
-    secured: true
+  blockchain: 'eos',
+  protocol: 'https',
+  host: 'dconnect.live',
+  port: 443,
+  chainId: '342f1b4af05f4978a8f5d8e3e3e3761cb22dacf21a93e42abe0753bdacb6b621',
+  secured: true
 }
 window.config = {
-      chainId: network.chainId, // 32 byte (64 char) hex string          
-      expireInSeconds: 60,
-      httpEndpoint: "http" + (network.secured ? 's' : '') + '://'  + network.host + ':' + network.port
-    };
+  chainId: network.chainId, // 32 byte (64 char) hex string          
+  expireInSeconds: 60,
+  httpEndpoint: "http" + (network.secured ? 's' : '') + '://'  + network.host + ':' + network.port
+};
 
     window.eosPublic = new window.Eos(config);
 console.log($('#search'));
