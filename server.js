041cdc068a71d3bff237876026b8bb9974a43c26
Glitch (messaround) <none>
2019-05-11 21:55:07 +0000

    🎷👜 Checkpoint ./server.js:115759/1498 ./package.json:115759/787

--- server.js
+++ server.js
@@ -19,10 +19,38 @@ const server = http.createServer(app);
 var MongoClient = require('mongodb').MongoClient;
 // This creates our socket using the instance of the server
 
+var util = require('util')
+var stream = require('stream')
+var Transform = stream.Transform
+
+util.inherits(Stringer, Transform)
+
+function Stringer() {
+  Transform.call(this, { objectMode: true } )
+  // 'object mode allows us to consume one object at a time
+
+}
+
+Stringer.prototype._transform = function(chunk, encoding, cb) {
+  var pretty = JSON.stringify(chunk, null, 2) 
+  this.push(pretty) // 'push' method sends data down the pike.
+  cb() // callback tells the incoming stream we're done processing 
+}
+
+var ss = new Stringer()
+
+
 MongoClient.connect(process.env.url, { useNewUrlParser: true,reconnectTries: 60, reconnectInterval: 1000}, async function(err, db) {
   let dbo = db.db("dconnectlive");
   let collection = await dbo.collection("transactions");
-  const changeStreamCursor = collection.watch([]);
+  const changeStreamCursor = collection.watch();
+  
+  app.get('/transactions', (req, res) => {
+    collection.find()
+      .pipe(require('JSONStream').stringify())
+      .pipe(res.type('json'))
+  })
+  
   function pollStream(cursor) {
     while (!cursor.isExhausted()) {
       if (cursor.hasNext()) {
@@ -40,9 +68,6 @@ MongoClient.connect(process.env.url, { useNewUrlParser: true,reconnectTries: 60,
     }
   };
   // disconnect is fired when a client leaves the server
-  socket.on("disconnect", () => {
-    console.log("user disconnected");
-  });
 });
 
 /* Below mentioned steps are performed to return the Frontend build of create-react-app from build folder of backend Comment it out if running locally*/
