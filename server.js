3bb3c80eb1736931014b81e24a5c5ffdd51c0e6b
Glitch (messaround) <none>
2019-05-11 22:17:06 +0000

    🎒⛵️ Checkpoint ./server.js:115759/2465 ./package.json:115759/25

--- server.js
+++ server.js
@@ -1,44 +1,11 @@
 const express = require("express");
 const http = require("http");
-const socketIO = require("socket.io");
-
-// Connection string of MongoDb database hosted on Mlab or locally
-
 var connection_string = process.env.url;
-
-
-// our localhost port
 const port = process.env.PORT || 3000;
-
 const app = express();
-
 app.use(express.static('public'));
-// our server instance
 const server = http.createServer(app);
-
 var MongoClient = require('mongodb').MongoClient;
-// This creates our socket using the instance of the server
-
-var util = require('util')
-var stream = require('stream')
-var Transform = stream.Transform
-
-util.inherits(Stringer, Transform)
-
-function Stringer() {
-  Transform.call(this, { objectMode: true } )
-  // 'object mode allows us to consume one object at a time
-
-}
-
-Stringer.prototype._transform = function(chunk, encoding, cb) {
-  var pretty = JSON.stringify(chunk, null, 2) 
-  this.push(pretty) // 'push' method sends data down the pike.
-  cb() // callback tells the incoming stream we're done processing 
-}
-
-var ss = new Stringer()
-
 
 MongoClient.connect(process.env.url, { useNewUrlParser: true,reconnectTries: 60, reconnectInterval: 1000}, async function(err, db) {
   let dbo = db.db("dconnectlive");
@@ -46,7 +13,7 @@ MongoClient.connect(process.env.url, { useNewUrlParser: true,reconnectTries: 60,
   const changeStreamCursor = collection.watch();
   
   app.get('/transactions', (req, res) => {
-    collection.find()
+    collection.find().sort({_id:-1})
       .pipe(require('JSONStream').stringify())
       .pipe(res.type('json'))
   })
@@ -54,13 +21,6 @@ MongoClient.connect(process.env.url, { useNewUrlParser: true,reconnectTries: 60,
   changeStreamCursor.on('change', next => {
     console.log(next);  
   });
-  let updateOps = {
-    $match: {
-      $and: [
-        { operationType: "insert" }
-      ]
-    }
-  };
   // disconnect is fired when a client leaves the server
 });
 
