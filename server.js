7aaf158c1f0097673e77644f63676832ddc2bdf5
Glitch (messaround) <none>
2019-05-11 21:44:09 +0000

    🚨🚊 Checkpoint ./server.js:115759/6165

--- server.js
+++ server.js
@@ -6,38 +6,39 @@ const socketIO = require("socket.io");
 
 var connection_string = process.env.url;
 
-// Collection name should be "FoodItems", only one collection as of now.
-
-// Document format should be as mentioned below, at least one such document:
-
-// {
-//     "_id": {
-//         "$oid": "5c0a1bdfe7179a6ca0844567"
-//     },
-//     "name": "Veg Roll",
-//     "predQty": 100,
-//     "prodQty": 295,
-//     "ordQty": 1
-// }
-
-const db = require("monk")(connection_string);
-
-const collection_transactions = db.get("transactions");
 
 // our localhost port
 const port = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(express.static('public'));
 // our server instance
 const server = http.createServer(app);
 
+var MongoClient = require('mongodb').MongoClient;
 // This creates our socket using the instance of the server
-const io = socketIO(server);
 
-io.on("connection", socket => {
-  console.log("New client connected" + socket.id);
-
+MongoClient.connect(process.env.url, { useNewUrlParser: true,reconnectTries: 60, reconnectInterval: 1000}, async function(err, db) {
+  let dbo = db.db("dconnectlive");
+  let collection = await dbo.collection("transactions");
+  const changeStreamCursor = collection.watch([]);
+  function pollStream(cursor) {
+    while (!cursor.isExhausted()) {
+      if (cursor.hasNext()) {
+        const change = cursor.next();
+        console.log(JSON.stringify(change));
+      }
+    }
+    pollStream(cursor);
+  }
+  let updateOps = {
+    $match: {
+      $and: [
+        { operationType: "insert" }
+      ]
+    }
+  };
   // disconnect is fired when a client leaves the server
   socket.on("disconnect", () => {
     console.log("user disconnected");
@@ -46,6 +47,9 @@ io.on("connection", socket => {
 
 /* Below mentioned steps are performed to return the Frontend build of create-react-app from build folder of backend Comment it out if running locally*/
 
-app.use(express.static('public'));
 
-server.listen(port, () => console.log(`Listening on port ${port}`));
+  const listener = app.listen(process.env.PORT, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+  }); 
+
+app.use(express.static('public'));
