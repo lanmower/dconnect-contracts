b3859c0a2a76d60d458c574dee5bbb5295aa88ef
Glitch (dconnect-homepage) <none>
2019-05-11 20:49:08 +0000

    🙌👱 Checkpoint ./server.js:115759/3864

--- server.js
+++ server.js
@@ -2,35 +2,36 @@ const express = require('express');
 const app = express();
 
 app.use(express.static('public'));
-const Mongo = require('mongodb').MongoClient;
-const conn = new Mongo("mongodb://localhost:27017/demo?replicaSet=rs");
-const db = conn.getDB("demo");
-const collection = db.stock; 
 
-let updateOps = {
-  $match: {
-    $and: [
-      { "updateDescription.updatedFields.quantity": { $lte: 10 } },
-      { operationType: "update" }
-    ]
-  }
-};
+var MongoClient = require('mongodb').MongoClient;
 
-const changeStreamCursor = collection.watch([updateOps]);
 
-pollStream(changeStreamCursor);
-
-//this function polls a change stream and prints out each change as it comes in
-function pollStream(cursor) {
-  while (!cursor.isExhausted()) {
-    if (cursor.hasNext()) {
-      change = cursor.next();
-      print(JSON.stringify(change));
+MongoClient.connect(process.env.url, { useNewUrlParser: true,reconnectTries: 60, reconnectInterval: 1000}, async function(err, db) {
+  let dbo = db.db("dconnectlive");
+  let collection = await dbo.collection("transactions");
+  
+  let updateOps = {
+    $match: {
+      $and: [
+        { operationType: "insert" }
+      ]
     }
-  }
-  pollStream(cursor);
-}
+  };
 
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-}); 
+  const changeStreamCursor = collection.watch([updateOps]);
+
+
+  pollStream(changeStreamCursor);
+  //this function polls a change stream and prints out each change as it comes in
+  function pollStream(cursor) {
+    while (cursor.hasNext()) {
+        const change = cursor.next();
+        print(JSON.stringify(change));
+    }
+    pollStream(cursor);
+  }
+
+  const listener = app.listen(process.env.PORT, function() {
+    console.log('Your app is listening on port ' + listener.address().port);
+  }); 
+});
