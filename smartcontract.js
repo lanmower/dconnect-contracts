b1ac2be879d7b79e5f7dd0a0e91a5132cada7075
Glitch (messaround) <none>
2019-05-13 00:04:10 +0000

    🙋🐊 Checkpoint ./smartcontract.js:115759/13084

--- smartcontract.js
+++ smartcontract.js
@@ -22,71 +22,55 @@ class SmartContracts {
           events: [],
         },
       };
+      console.log('tet');
       MongoClient.connect(process.env.url, { useNewUrlParser: true,reconnectTries: 60, reconnectInterval: 1000}, async function(err, db) {
         let dbo = db.db("dconnectlive");
-        let collection = await dbo.collection("transactions");
-      });
-      const rng = seedrandom(`${id}`);
-      // prepare the db object that will be available in the VM
-      const db = {
-        find: (table, query) => ()=>{
-        },
-        findInContract: (contractName, table, query) =>{
-        },
-        findOne: (table, query) => {
-        },
-        findOneInContract: (contractName, table, query) =>{
-        },
-        insert: (table, record) => ()=>{
-        },
-        remove: (table, record) => ()=>{
-        },
-        update: (table, record) => ()=>{
-        },
-      };
-      // initialize the state that will be available in the VM
-      const vmState = {
-        api: {
-          sender,
-          id,
-          action,
-          payload: JSON.parse(JSON.stringify(payload)),
-          random: () => rng(),
-          debug: log => console.log(log), // eslint-disable-line no-console
-          // emit an event that will be stored in the logs
-          emit: (event, data) => typeof event === 'string' && results.logs.events.push({ contract, event, data }),
-          // add an error that will be stored in the logs
-          assert: (condition, error) => {
-            if (!condition && typeof error === 'string') {
-              results.logs.errors.push(error);
-            }
-            return condition;
+        let collection = await dbo.collection(contract);
+        const rng = seedrandom(`${id}`);
+        // initialize the state that will be available in the VM
+        const vmState = {
+          api: {
+            sender,
+            id,
+            action,
+            collection,
+            fromCollection:async (contract)=>{return (await dbo.collection(contract)).find},
+            payload: JSON.parse(JSON.stringify(payload)),
+            random: () => rng(),
+            debug: log => console.log(log), // eslint-disable-line no-console
+            // emit an event that will be stored in the logs
+            emit: (event, data) => typeof event === 'string' && results.logs.events.push({ contract, event, data }),
+            // add an error that will be stored in the logs
+            assert: (condition, error) => {
+              if (!condition && typeof error === 'string') {
+                results.logs.errors.push(error);
+              }
+              return condition;
+            },
           },
-        },
-      };
-      try {
-        const payload = JSON.parse(transaction.payload);
+        };
+        try{
+        payload = JSON.parse(transaction.payload);
+        } catch(e) {}
         if(transaction.contract == 'system' && transaction.action == 'setcontract') {
           console.log('setting contract', payload.name, payload.code);
           contracts[payload.name] = payload.code;
         }
-      } catch (e) {
-      }
-      console.log(contract, contracts);
-      if(!contracts[contract]) return results;
-      console.log('running contract');
-      const error = await SmartContracts.runContractCode(vmState, contracts[contract], jsVMTimeout);
-      if (error) {
-        const { name, message } = error;
-        if (name && typeof name === 'string'
-            && message && typeof message === 'string') {
-          return { logs: { errors: [`${name}: ${message}`] } };
+        console.log(contract, contracts);
+        if(!contracts[contract]) return results;
+        console.log('running contract');
+        const error = await SmartContracts.runContractCode(vmState, contracts[contract], jsVMTimeout);
+        if (error) {
+          const { name, message } = error;
+          if (name && typeof name === 'string'
+              && message && typeof message === 'string') {
+            return { logs: { errors: [`${name}: ${message}`] } };
+          }
+          return { logs: { errors: ['unknown error'] } };
         }
-        return { logs: { errors: ['unknown error'] } };
-      }
-      return results;
+        return results;
+      });
     } catch (e) {
-      // console.error('ERROR DURING CONTRACT EXECUTION: ', e);
       console.log('error', e); 
       return { logs: { errors: [`${e.name}: ${e.message}`] } };
     }
