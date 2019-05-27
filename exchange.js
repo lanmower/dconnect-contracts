2e59cfbe4e4d315c1d0b7b2ea53b3e6bdcc5bc2f
root <root@vps210556.vps.ovh.ca>
2019-05-27 13:04:45 -0400

    timestamp fix

--- exchange.js
+++ exchange.js
@@ -5,12 +5,29 @@ const http = require('http');
 const express = require("express");
 const port = process.env.PORT || 3000;
 const app = express();
-app.use(express.static('public'));
-const server = http.createServer(app);
+
+var expressWs = require('express-ws')(app);
+var clients = {};
+global.id = 0;
+app.ws('/', function(ws, req) {
+  ++global.id;
+  clients[id] = ws;
+  ws.on('message', function(msg) {
+    ws.send(msg);
+  });
+  ws.onclose = ()=> {
+    delete clients[ws];
+  }
+  update.push(
+   ws
+  );
+  ws.send(msg);
+}); 
+
 var MongoClient = require('mongodb').MongoClient;
 var db = null, rdb = null;
 const smartcontracts = require('./smartcontract.js').SmartContracts;
-console.log(process.env.url);
+
 MongoClient.connect("mongodb://localhost/admin", { useNewUrlParser: true, reconnectTries: 60, reconnectInterval: 2000}, async function(err, dbi) {
 MongoClient.connect(process.env.url, { useNewUrlParser: true, poolSize:1, reconnectTries: 60, reconnectInterval: 2000}, async function(err, rdbi) {
   console.log(err);
@@ -18,9 +35,13 @@ MongoClient.connect(process.env.url, { useNewUrlParser: true, poolSize:1, reconn
   rdb = rdbi;  
   let rdbo = rdb.db("dconnectlive");
   let dbo = db.db("dconnectlive");
-  app.get('/db/*', async (req, res) => {
-    const collection = await dbo.collection(req.path.replace('/db/',''));
-    collection.find().sort({_id:-1})
+
+   app.get('/db/*', async (req, res) => {
+const name = req.path.replace('/db/','');
+
+	  const col = await dbo.collection(name);
+//console.log(await col.findOne());
+	  col.find().sort({_id:-1})
       .pipe(require('JSONStream').stringify())
       .pipe(res.type('json'));
   })
@@ -37,7 +58,7 @@ MongoClient.connect(process.env.url, { useNewUrlParser: true, poolSize:1, reconn
   const processedData = (await processed.findOne())||{timestamp:new Date(0)};
   const afterTime = processedData?processedData.timestamp:0;
   let cursor = collection.find({timestamp:{$exists:true}, account:'dconnectlive', timestamp:{$gt:new Date(afterTime)}}).sort({timestamp:1});
-console.log(cursor.count());
+//console.log(cursor.count());
   async function run(item) { 
     const before = new Date().getTime();
     const date = new Date(item.timestamp).getTime();
@@ -51,7 +72,15 @@ console.log(cursor.count());
     }, 2000, dbo); 
     //console.log(new Date().getTime()-before);
     await logs.insert({id:item.transactionId, res, timestamp:item.timestamp}); 
-    await processed.update({}, {timestamp:item.timestamp}, {upsert:true}); 
+    await processed.update({}, {timestamp:item.timestamp}, {upsert:true});
+    Object.keys(clients).forEach((ws)=>{
+	//console.log(clients[ws].readyState);
+	if(clients[ws].readyState == 3) delete clients[ws];
+	else {
+		//console.log('sending to websocket');
+		clients[ws].send(JSON.stringify(res));
+	}
+    });
     //console.log(item, res);
   }  
   const total = await cursor.count();
@@ -62,7 +91,7 @@ console.log(cursor.count());
     //console.log(total-(count++));
   }
   collection.watch({account:'dconnectlive'}).on('change', async (next) => {
-    run(next.fullDocument);
+    await run(next.fullDocument);
   });
 }); 
 }); 
@@ -70,7 +99,6 @@ const listener = app.listen(port, function() {
   console.log('Your app is listening on port ' + listener.address().port);
 });
 app.use(express.static('public'));
-
 
 // Create a function to terminate your app gracefully:
 function gracefulShutdown(){
@@ -83,7 +111,7 @@ function gracefulShutdown(){
     });
 }
 
-process.stdin.resume();//so the program will not close instantly
+process.stdin.resume();
 process.on('exit', gracefulShutdown);
 process.on('SIGINT', gracefulShutdown);
 process.on('SIGTERM', gracefulShutdown);
